265100f9d8993fb22f410b16f81392ef1c539eb1
Sujin Chaudhary <idkifk237@gmail.com>
2022-07-28 09:53:34 +0545

    Github link for portfolio

--- Portfolio_react_js/src/Components/jsx/Portfolio.jsx
+++ Portfolio_react_js/src/Components/jsx/Portfolio.jsx
@@ -4,34 +4,6 @@ import img2 from '../../assets/fashion.png'
 import img3 from '../../assets/restro.png'
 
 
-/* const data=[
-  { "id": 5,
-   "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-    "price": 695,
-     "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.", 
-     "category": "jewelery", "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg", 
-     "rating": { "rate": 4.6, "count": 400 } },
-  { "id": 6,
-   "title": "Solid Gold Petite Micropave ",
-    "price": 168,
-     "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-     "category": "jewelery", "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg", 
-     "rating": { "rate": 3.9, "count": 70 } },
-  { "id": 7,
-   "title": "White Gold Plated Princess"
-   , "price": 9.99,
-    "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...", 
-    "category": "jewelery", "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg", 
-    "rating":{ "rate": 3, "count": 400 } },
-  { "id": 8,
-   "title": "Pierced Owl Rose Gold Plated Stainless Steel Double",
-    "price": 10.99, 
-    "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-     "category": "jewelery", "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
-      "rating": { "rate": 1.9, "count": 100 } }
-]
- */
-
 
 export default function Portfolio() {
   return (
@@ -42,18 +14,7 @@ export default function Portfolio() {
 
 
       <div className="container portfolio_container">
-       {/*  {
-          data.map(({id,title,price}) =>{
-            return(
-              <article key={id} className="test">
-                <div className="image-item">
-                  <div className="price">{price}</div>
-                  <h3>{title}</h3>
-                </div>
-              </article>
-            )
-          })
-        } */}
+       
        {/*  Restaurant Portfolio */}
         <article className='portfolio_content'>
           <div className="portfolio_item">
@@ -61,7 +22,7 @@ export default function Portfolio() {
               <img src={img3} alt="" />
             </div>
             <h3>Restaurant Website</h3>
-            <a href="https://github.com" className='btn btn-primary'>Github</a>    
+            <a href="https://github.com/sujin-chaudhary/Own-Portfolio.git" className='btn btn-primary' target={_blank} rel="noopener noreferrer">Github</a>    
           </div>
         </article>
 
@@ -72,7 +33,7 @@ export default function Portfolio() {
               <img src={img2} alt="" />
             </div>
             <h3>Fashion Website</h3>
-            <a href="https://github.com" className='btn btn-primary'>Github</a>    
+            <a href="https://github.com/sujin-chaudhary/Own-Portfolio.git" className='btn btn-primary' target={_blank} rel="noopener noreferrer">Github</a>    
           </div>
         </article>
 
@@ -83,7 +44,7 @@ export default function Portfolio() {
               <img src={img1} alt="" />
             </div>
             <h3>AnimeOwl Website</h3>
-            <a href="https://github.com" className='btn btn-primary'>Github</a>    
+            <a href="https://github.com/sujin-chaudhary/Own-Portfolio.git" className='btn btn-primary' target={_blank} rel="noopener noreferrer">Github</a>    
           </div>
         </article>
 
